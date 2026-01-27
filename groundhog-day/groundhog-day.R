88706969b4dcf197f737de5a60bffccc4f455e7b
Erin Zylstra <erinzylstra@gmail.com>
2026-01-27 10:53:39 -0700

    Update script for 2026, using PRISM AGDD data

--- groundhog-day/groundhog-day.R
+++ groundhog-day/groundhog-day.R
@@ -1,6 +1,16 @@
 # Groundhog day script
 # Adapted from https://github.com/alyssarosemartin/spring-media/blob/main/groundhog/groundhog.R#L1
-# 31 Jan 2025
+# 27 Jan 2026
+
+# 2026 notes: 
+# We're no longer producing AGDD forecasts for the next 7 days because we're 
+# just using PRISM data. So the maps will reflect AGDD values as the end of 
+# January.
+# I added code to fill the state code when missing, which added ~10% more sites.
+
+# General reminder: like in the past, we haven't restricted observations to 
+# those with a prior no. Map includes all first yeses, regardless of whether 
+# there was a prior no or not.
 
 library(here)
 library(rnpn)
@@ -14,7 +24,7 @@ library(ggimage)
 # Set parameters --------------------------------------------------------------#
 
 # Year
-year <- 2025
+year <- 2026
 
 # Define breaks that will be used to delineate areas cooler/warmer than 30-year
 # normals. Breaks = AGDD anomolies on Groundhog day (deg F).
@@ -32,6 +42,21 @@ layers <- npn_get_layer_details()
 phenoclasses <- npn_pheno_classes() %>% data.frame()
 # phenoclasses
 
+# Get states layer ------------------------------------------------------------#
+
+# File location
+states_shp <- here("shapefiles", "us_states.shp")
+
+# If file exists, load it. Otherwise download it first.
+if (!file.exists(states_shp)) {
+  library(rnaturalearth)
+  states <- rnaturalearth::ne_states(country = "united states of america", 
+                                     returnclass = "sv")
+  states <- states[, "postal"]
+} else {
+  states <- terra::vect(states_shp)
+}
+
 # Aquire, format phenometric data ---------------------------------------------#
 
 # Download individual phenometrics (first yeses) so far this year: 
@@ -42,14 +67,25 @@ df <- npn_download_individual_phenometrics(
   pheno_class_ids = c(1, 7)
 )
 
-# Remove observations outside the continental US (for now, assuming
-# that if state is missing, location is in lower 48)
+# Some observations missing state ID. Will fill in using states layer from the
+# tigris package adn then restrict observations to continental US
 states48 <- state.abb[! state.abb %in% c("AK", "HI")]
-df48 <- df %>%
-  filter(state %in% c(states48, "-9999"))
+state_fill <- filter(df, is.na(state)) %>%
+  select(site_id, longitude, latitude) %>%
+  rename(lon = longitude, lat = latitude) %>%
+  distinct()
+state_fillv <- vect(state_fill, crs = "epsg:4326")
+state_new <- terra::extract(states, state_fillv)
+state_fill <- cbind(state_fill, state_new = state_new$postal)
+df <- df %>%
+  left_join(select(state_fill, site_id, state_new), by = "site_id") %>%
+  mutate(state = ifelse(!is.na(state), state, state_new)) %>%
+  select(-state_new) %>%
+  # Restrict observations to the continental US
+  filter(state %in% states48)
 
 # Simplify first observation data
-df48 <- df48 %>%
+df <- df %>%
   select(site_id, latitude, longitude, elevation_in_meters, state, 
          common_name, individual_id, phenophase_description, pheno_class_id,
          first_yes_doy, numdays_since_prior_no, last_yes_doy, 
@@ -63,72 +99,82 @@ df48 <- df48 %>%
          prior_no = numdays_since_prior_no,
          last_yes = last_yes_doy,
          next_no = numdays_until_next_no) %>%
-  mutate(prior_no = ifelse(prior_no == -9999, NA, prior_no),
-         next_no = ifelse(next_no == -9999, NA, next_no),
-         elev = ifelse(elev == -9999, NA, elev),
-         state = ifelse(state == "-9999", NA, state)) %>%
   data.frame()
 
 # Look at species included
-count(df48, common_name)
-# Delete any 'ohi'a lehua records
-df48 <- df48 %>%
+count(df, common_name)
+# Delete any 'ohi'a lehua records, if present
+df <- df %>%
   filter(common_name != "'ohi'a lehua")
 
 # Delete any observations after groundhog day (in case we're running this 
 # after the fact)
-groundhog_doy <- yday(ymd(paste0(year, "-02-02")))
-df48 <- df48 %>%
+groundhog_date <- ymd(paste0(year, "-02-02"))
+groundhog_doy <- yday(groundhog_date)
+df <- df %>%
   filter(first_yes <= groundhog_doy)
 
 # Distribution of first observation dates
-ggplot(df48) +
+ggplot(df) +
   geom_histogram(aes(x = first_yes)) +
   facet_wrap(~pheno_class_id, ncol = 1)
 
 # How many observations and individuals?
-df48 %>%
+df %>%
   group_by(pheno_class_id) %>%
   summarize(n_obs = n(),
             n_indiv = n_distinct(id))
 
 # Keep just one "first yes" for each plant and phenophase class 
-df48 <- df48 %>%
+df <- df %>%
   arrange(pheno_class_id, common_name, id, first_yes) %>%
   distinct(pheno_class_id, id, .keep_all = TRUE)
 
 # How many observations, and what proportion have prior no?
-df48 %>%
+df %>%
   group_by(pheno_class_id) %>%
   summarize(n_obs = n(),
             with_prior_no = sum(is.na(prior_no))) %>%
   data.frame() %>%
   mutate(prop_prior_no = with_prior_no / n_obs)
 
-leaf <- subset(df48, pheno_class_id == 1)
-flower<- subset(df48, pheno_class_id == 7)
+leaf <- subset(df, pheno_class_id == 1)
+flower<- subset(df, pheno_class_id == 7)
 
-# Aquire forecasted AGDD anomalies --------------------------------------------#
+# Aquire AGDD anomalies -------------------------------------------------------#
 
-# Acquire raster forecast anomaly for Feb 2 (32 base)
+# Acquire raster forecast anomaly for Feb 2 (32 base) or most recent date when
+# AGDD data are available
 layers %>%
   filter(name == "gdd:agdd_anomaly") %>%
   select(name, title, abstract, dimension.name)
 
-gh_day <- paste0(year, "-02-02")
+today <- ymd(Sys.Date())
+agdd_day <- min(c(today, groundhog_date))
+
 groundhog <- npn_download_geospatial(coverage_id = "gdd:agdd_anomaly", 
-                                     date = gh_day)  
+                                     date = agdd_day)  
+# If max value in raster is 0, then there's no data for that date. Go back one
+# day
+max_value <- terra::global(groundhog, fun = max, na.rm = TRUE)
+if (max_value == 0) {
+  groundhog <- npn_download_geospatial(coverage_id = "gdd:agdd_anomaly", 
+                                       date = agdd_day - 1)  
+}
+
 # plot(groundhog)
 # hist(groundhog)
+
+# Convert map to WGS84, which is datum that NPN data are in
+groundhog <- terra::project(groundhog, "epsg:4326")
 
 # Create map ------------------------------------------------------------------#
 
 # Note: flower and leaf icons already downloaded into resources folder
 # (leaf.svg, flower.ico)
 
-# Convert to spatraster and bin data
-ghd <- rast(groundhog)
-ghd_class <- terra::classify(x = ghd, 
+# Bin data
+ghd_class <- terra::classify(x = groundhog, 
                              rcl = breaks, 
                              include.lowest = TRUE)
 freq(ghd_class)
@@ -151,7 +197,7 @@ ghd_plot <- ggplot() +
 ghd_plot
 
 ghd_filename <- here("groundhog-day/output",
-                     paste0("groundhog-day", year, ".png"))
+                     paste0("groundhog-day-", year, ".png"))
 
 # Save to file (commented out for now, so we don't accidentally overwrite anything)
 # ggsave(filename = ghd_filename,
